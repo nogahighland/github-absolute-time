fa98b679a1bb81426433d6e81e8f51615e926b72
kisshy0601 <hiroki_kishi@fablic.co.jp>
2020-06-24 10:36:59 +0900

    fix timing

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ function repplaceRelativeTime() {
   const bgColor = $($('.TableObject-item > .State')[0]).css('background-color') || '#00a497';
   $('relative-time').each(function() {
     const absTime = moment($(this).attr('datetime')).format('llll');
-    $(this).html(absTime).css({ backgroundColor: bgColor, color: 'white', borderRadius: '3px', padding: '2px 3px', fontSize: 'smaller' });
+    const height = $(this).height();
+    $(this).html(absTime).css({ backgroundColor: bgColor, color: 'white', borderRadius: `${height/2}px`, padding: '2px 4px', fontSize: 'smaller' });
   });
 }
-repplaceRelativeTime();
-setInterval(repplaceRelativeTime, 60 * 1000);
+setTimeout(repplaceRelativeTime, 0.5 * 1000);
+setInterval(repplaceRelativeTime, 10 * 1000);
