28e01e1a8f74a81808e845bfde9a3e4e7bb4cc7e
kissy <hiroki.kishi@finatext.com>
2023-01-26 14:42:56 +0900

    fix great time

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 function repplaceRelativeTime() {
   const bgColor = $($('.TableObject-item > .State')[0]).css('background-color') || '#00a497';
-  $('relative-time').each(function() {
+  $('relative-time').each(function () {
     const absTime = moment($(this).attr('datetime')).format('llll');
     const height = $(this).height();
-    $(this).html(absTime).css({ backgroundColor: bgColor, color: 'white', borderRadius: `${height/2}px`, padding: '2px 4px', fontSize: 'smaller' });
+    this.shadowRoot.innerHTML = absTime
+    $(this).css({backgroundColor: bgColor, color: 'white', borderRadius: `${height / 2}px`, padding: '2px 4px', fontSize: 'smaller'});
   });
 }
 setTimeout(repplaceRelativeTime, 0.5 * 1000);
